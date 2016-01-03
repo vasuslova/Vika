2f63aa91225a9850fa39149133d1e35fe49b05f0
Vika Suslova <suslik.vik@gmail.com>
2016-01-03 16:10:31 +0300

    Manager has been created

--- local/modules/miet.vika/lib/manager.php
+++ local/modules/miet.vika/lib/manager.php
@@ -1,7 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Валера
- * Date: 03.01.2016
- * Time: 14:16
- */
+namespace MIET\NEON;
+use Bitrix\Main\Application;
+use Bitrix\Main\Entity;
+use Bitrix\Main\Entity\Event;
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Iblock\ElementTable;
+use Bitrix\Main\UserTable;
+use MIET\VIKA\FilmTable;
+use MIET\VIKA\CinemaTable;
+use MIET\VIKA\ProviderTable;
+
+Loc::loadMessages(__FILE__);
+class FilmManager
+{
+    public static function SaveFilm($arData)
+    {
+        $db = Application::getConnection();
+        $db->startTransaction();
+        $result = FilmTable::add(array(
+            "UF_NAME" => $arData["NAME"],
+            "UF_CINEMA" => $arData["CINEMA"],
+            "UF_PROVIDER" => $arData["PROVIDER"],
+        ));
+        if (!$result->isSuccess()) {
+            $db->rollbackTransaction();
+            return false;
+        }
+        //$idFilm = $result->getId();
+
+        if ($result->isSuccess()) {
+            $db->commitTransaction();
+            return true;
+        } else {
+            $db->rollbackTransaction();
+            return false;
+        }
+    }
+}
